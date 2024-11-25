55c1f1375fbe93ced5e75fe823156c0224536627
Petrik Diak <diak@petrik.hu>
2024-11-25 10:13:42 +0100

    nem jó a backend

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,38 @@
-const express = require("express")
-const mysql = require("mysql2/promise")
+import express from 'express'
+import mysql from 'mysql2'
+import cors from 'cors'
 const app = express()
-const PORT = 3000
-const db = await mysql.createConnection({
+app.use(cors())
+app.use(express.json())
+
+const db = mysql.createPool({
     host: "localhost",
     user: "root",
     database: "sajtok",
-    password: ""
+    password: ''
+})
+
+//BOGÁRDI BENEDEK
+//getAll, getOne, post
+app.get('/', (req, res) => {
+    
+        return res.status(200, "segg")
+
+        /*const temp = await db.query('SELECT * FROM sajtok');
+        const rows = temp[0];
+        const fields = temp[1];
+        return res.status(200).json(rows);
+    } catch (error) {
+        console.error(`Error retrieving phones ${error}`);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }*/
 })
 
 
-app.listen(PORT, (error) => {
-    if (!error) {
-        console.log(`Szerver fut a :${PORT} porton`)
+app.listen(3000, err => {
+    if (err) {
+      console.log(err);
+      return process.exit(1);
     }
-    else throw error
-})
+    console.log(`Server is running on ${3000}`);
+  });
